45f0216ad299dd40c2ef4dacf11d3cb565305e1f
John Ovalle <johnovalle@gmail.com>
2017-09-20 15:24:41 -0400

    exploring visual studio and intro to c#

---
+++ Hello/Hello/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Hello
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter your name");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("How many hours of sleep did you get last night?:");
+            int hoursOfSleep = int.Parse(Console.ReadLine());
+
+            if(hoursOfSleep > 7)
+            {
+                Console.WriteLine("Well rested aren't you " + userName +"?");
+            }
+            else
+            {
+                Console.WriteLine("Go get some rest " + userName);
+            }
+
+            //Console.WriteLine("Hello " + userName);
+
+        }
+    }
+}
