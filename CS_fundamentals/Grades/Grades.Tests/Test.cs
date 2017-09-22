197f456bc5ff2bc9eaf062e91b5565d685206e58
John Ovalle <johnovalle@gmail.com>
2017-09-22 19:04:25 -0400

    add unit tests

---
+++ CS_fundamentals/Grades/Grades.Tests/Test.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+using System;
+namespace Grades.Tests
+{
+    [TestFixture()]
+    public class Test
+    {
+        [Test()]
+        public void TestCase()
+        {
+            Assert.AreEqual(3,3);
+        }
+    }
+}
