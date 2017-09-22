f427eac46d4b2a773d4633e536a02e06ff4c0fbb
John Ovalle <johnovalle@gmail.com>
2017-09-22 16:07:17 -0400

    classes and objects module

---
+++ CS_fundamentals/Grades/Grades/GradeBook.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Grades
+{
+    public class GradeBook
+    {
+        private List<float> grades = new List<float>();
+
+        public GradeBook()
+        {
+            
+        }
+        public void AddGrade(float grade) 
+        {
+            grades.Add(grade);
+        }
+
+        public GradeStatistics ComputeStatistics()
+        {
+            GradeStatistics stats = new GradeStatistics();
+
+            float sum = 0;
+            foreach (float grade in grades)
+            {
+                //if(grade > stats.HighestGrade)
+                //{
+                //    stats.HighestGrade = grade;
+                //}
+                stats.HighestGrade = Math.Max(stats.HighestGrade, grade);
+                //if(grade < stats.LowestGrade)
+                //{
+                //    stats.LowestGrade = grade;
+                //}
+                stats.LowestGrade = Math.Min(stats.LowestGrade, grade);
+                sum += grade;
+            }
+
+            stats.AverageGrade = sum / grades.Count;
+
+            return stats;
+        }
+
+
+
+
+
+    }
+}
---
+++ CS_fundamentals/Grades/Grades/Properties/GradeBook.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace Grades.Properties
+{
+    public class GradeBook
+    {
+        public GradeBook()
+        {
+        }
+    }
+}
